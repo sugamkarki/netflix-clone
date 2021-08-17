0e9d9c460cfa0e73a33602be85a0857749518619
sugam <sugamkarki7058@gmail.com>
2021-08-17 18:19:09 +0545

    finished

---
+++ src/components/Row.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+import axios from "../axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* several row posters */}
+
+        {movies.map((movie) => {
+          //   if (movie.backdrop_path === undefined) {
+          //     console.log(title);
+          //   }
+          //   console.log(movie.backdrop_path);
+          return (
+            <img
+              key={movie?.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+              src={`${base_url}${
+                isLargeRow ? movie?.poster_path : movie?.backdrop_path
+              }`}
+              alt={movie.name}
+              onClick={() => {
+                if (trailerUrl) {
+                  setTrailerUrl("");
+                } else {
+                  //   setTrailerUrl("");
+                  movieTrailer(movie?.name || "")
+                    .then((url) => {
+                      //   setTrailerUrl();
+                      const urlParams = new URLSearchParams(
+                        new URL(url).search
+                      );
+                      setTrailerUrl(urlParams.get("v"));
+                    })
+                    .catch((error) => console.log(error));
+                }
+              }}
+            />
+          );
+        })}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
+// function movieTrailer(arg0) {
+//   throw new Error("Function not implemented.");
+// }
